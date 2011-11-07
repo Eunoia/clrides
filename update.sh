e6f87b8c421d4f7c5a0c7562e02750caa36a6002
Evan R <eunoia.github@gmail.com>
2011-11-06 21:14:49 -0800

    update now copies posts to salt/, the sinatra front end

--- update.sh
+++ update.sh
@@ -2,4 +2,4 @@ ruby clCache.rb
 ruby DichotomyDichotomy.rb
 ruby destinationEstimation.rb
 cp -v posts.sql ../NodeStuff/posts.sql
-cp -v posts.sql ../SinatraStuff/posts.sql
+cp -v posts.sql ../salt/posts.sql
