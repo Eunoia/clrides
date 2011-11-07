9a1844db1e107230bc013273712d96693f985456
Evan R <eunoia.github@gmail.com>
2011-11-06 21:21:36 -0800

    update.sh now works locally at remotely

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
-ruby clCache.rb
-ruby DichotomyDichotomy.rb
-ruby destinationEstimation.rb
-cp -v posts.sql ../NodeStuff/posts.sql
+cd craigslist-enhancement-suite 
+ruby clCache.rb &&
+ruby DichotomyDichotomy.rb && 
+ruby destinationEstimation.rb &&
+cp -v posts.sql ../NodeStuff/posts.sql &&
 cp -v posts.sql ../salt/posts.sql
